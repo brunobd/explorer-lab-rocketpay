2c0805b17dd5ecb26947a7c2e3d6befa23c29016
Bruno Duarte <bbduarte@live.com>
2022-10-25 07:43:28 -0300

    bug fix

--- src/main.js
+++ src/main.js
@@ -81,7 +81,9 @@ const cardHolder = document.querySelector("#card-holder")
 cardHolder.addEventListener("input", () => {
   const ccHolder = document.querySelector(".cc-holder .value")
   ccHolder.innerText =
-    cardHolder.value.length === 0 ? "FULANO DA SILVA " : cardHolder.value
+    cardHolder.value.length === 0
+      ? "FULANO DA SILVA "
+      : cardHolder.value.substring(0.29)
 })
 
 securityCodeMasked.on("accept", () => {
